428acc00d45be013c0af43b77c1d4a26b01d4ced
b2a3e8 <31370519+b2a3e8@users.noreply.github.com>
2019-04-30 11:04:20 +0200

    Make code a little prettier and implement some little TODOs

--- src/decode.js
+++ src/decode.js
@@ -1,8 +1,12 @@
 'use strict';
 
+/**
+ * Finds the matching boopadee for a bappadee.
+ * @param   {Array<string>}  array     Array of boopa-dee-bappa-dee commands to search in.
+ * @param   {Integer}        closePos  Pointer to bappadee command in command's array.
+ * @returns {Integer}                  Pointer to bappadee's matching boopadee command in command's array.
+ */
 function findMatchingOpeningBracket (array, closePos) {
-
-    // TODO function assumes that parentheses are balanced, so no array bounds checking is done. A real implementation would check that you don't run off the end of the array, and throw an exception (or return an error code) that indicates that parentheses are unbalanced in the input text if you do.
 
     var openPos = closePos;
     var counter = 1;
@@ -19,9 +23,13 @@ function findMatchingOpeningBracket (array, closePos) {
     return openPos;
 }
 
+/**
+ * Finds the matching bappadee for a boopadee.
+ * @param   {Array<string>}  array    Array of boopa-dee-bappa-dee commands to search in.
+ * @param   {Integer}        openPos  Pointer to boopadee command in command's array.
+ * @returns {Integer}                 Pointer to boopadee's matching bappadee command in command's array.
+ */
 function findMatchingClosingBracket (array, openPos) {
-
-    // TODO function assumes that parentheses are balanced, so no array bounds checking is done. A real implementation would check that you don't run off the end of the array, and throw an exception (or return an error code) that indicates that parentheses are unbalanced in the input text if you do.
 
     var closePos = openPos;
     var counter = 1;
@@ -40,23 +48,27 @@ function findMatchingClosingBracket (array, openPos) {
 
 
 /**
- * translate boopa-dee-bappa-dee to ascii
- * @param {string} string Boopa-dee-bappa-dee commands which should be translated.
- * @returns //TODO Promise
+ * Translates boopa-dee-bappa-dee to ascii
+ * @param   {string}           string      Boopa-dee-bappa-dee commands which should be translated.
+ * @returns {Promise<string>}              String of ASCII characters, decoded from boopa-dee-bappa-dee commands.
  */
 function decode (string) {
 
-    // TODO remove frequently used bullshit like newlines
+    return new Promise ((resolve, reject) => {
 
-    validate(string); // input validation (is boopa-dee-bappa-dee?)
+        // input validation (is boopa-dee-bappa-dee?)
+        try {
+            validate(string);
+        } catch (error) {
+            reject(error);
+        }
 
-    const input = string.split(" ");  // build an array of commands from string
-    var cursor = 0;                   // points to position in input array (to current command)
-    var data = [];                    // data store
-    var pointer = 0;                  // points to position in data store
-    var output = "";                  // decoded string
 
-    function interpret () { // TODO funktion loeschen
+        const input = string.split(" ");  // build an array of commands from string
+        var cursor = 0;                   // points to position in input array (to current command)
+        var data = [];                    // data store
+        var pointer = 0;                  // points to position in data store
+        var output = "";                  // decoded string
 
         // loop through input untill cursor points to the end
         while (cursor < input.length) {
@@ -100,34 +112,34 @@ function decode (string) {
             }
             cursor++;
         }
-    }
 
-    interpret();
+        resolve(output);
 
-    return output;
+    });
 }
 
 /**
- * validates a boopa-dee-bappa-dee string
- * @param {string} string Source code to execute.
- * @returns {boolean} boolean Boolean indicating whether source code is valid or not.
+ * Validates a boopa-dee-bappa-dee string.
+ * @param   {string}   string Source code to validate.
+ * @returns {boolean}         Indicating whether source code is valid or not.
  */
 function validate(string) {
 
-    // TODO check if brackets are balanced
+    // check if brackets are balanced
+    if (string.match(/(boopadee)/g).length !== string.match(/(bappadee)/g).length) {
+        throw new Error('source code is not valid: brackes are unbalanced!');
+    }
 
-    var regex = /^(boopa|bappa|pe|boo|dee|boopadee|bappadee| )+$/;
-
-    if (string.match(regex)) {
+    // check if string only contains valid commands
+    if (string.match(/^(boopa|bappa|pe|boo|dee|boopadee|bappadee| )+$/)) {
         return true; // source code is valid
     } else {
 
-        // source code is invalid
-        regex = /boopabappa/;
-        if (string.match(regex)) {
-            throw "source code is not valid! this translator does not support boopabappa command (accept one byte of input from stdin, storing its value in the byte at the data pointer), because the translators usage is intended for websites and you cant read from stdin there";
+        // check if string contains boopabappa command, to throw an more detailed error message
+        if (string.match(/boopabappa/)) {
+            throw new Error('source code is not valid: this translator does not support boopabappa command');
         } else {
-            throw "source code is not valid!";
+            throw new Error('source code is not valid!');
         }
     }
 }
--- tests/decode.spec.js
+++ tests/decode.spec.js
@@ -2,56 +2,48 @@ const decode = require('../src/decode');
 
 test('find matching opening bracket', () => {
     const input = 'pe pe pe pe pe pe pe pe boopadee boopa pe pe pe pe boopadee boopa pe pe boopa pe pe pe boopa pe pe pe boopa pe bappa bappa bappa bappa dee bappadee boopa pe boopa pe boopa dee boopa boopa pe boopadee bappa bappadee bappa dee bappadee boopa boopa boo boopa dee dee dee boo pe pe pe pe pe pe pe boo boo pe pe pe boo boopa boopa boo bappa dee boo bappa boo pe pe pe boo dee dee dee dee dee dee boo dee dee dee dee dee dee dee dee boo boopa boopa pe boo boopa pe pe boo'.split(" ");
-    var matchingBracket;
-
-    matchingBracket = decode.findMatchingOpeningBracket(input, 33);
-    expect(matchingBracket).toBe(14);
-
-    matchingBracket = decode.findMatchingOpeningBracket(input, 45);
-    expect(matchingBracket).toBe(43);
-
-    matchingBracket = decode.findMatchingOpeningBracket(input, 48);
-    expect(matchingBracket).toBe(8);
+    expect(decode.findMatchingOpeningBracket(input, 33)).toBe(14);
+    expect(decode.findMatchingOpeningBracket(input, 45)).toBe(43);
+    expect(decode.findMatchingOpeningBracket(input, 48)).toBe(8);
 });
 
 test('find matching closing bracket', () => {
     const input = 'pe pe pe pe pe pe pe pe boopadee boopa pe pe pe pe boopadee boopa pe pe boopa pe pe pe boopa pe pe pe boopa pe bappa bappa bappa bappa dee bappadee boopa pe boopa pe boopa dee boopa boopa pe boopadee bappa bappadee bappa dee bappadee boopa boopa boo boopa dee dee dee boo pe pe pe pe pe pe pe boo boo pe pe pe boo boopa boopa boo bappa dee boo bappa boo pe pe pe boo dee dee dee dee dee dee boo dee dee dee dee dee dee dee dee boo boopa boopa pe boo boopa pe pe boo'.split(" ");
-    var matchingBracket;
-
-    matchingBracket = decode.findMatchingClosingBracket(input, 14);
-    expect(matchingBracket).toBe(33);
-
-    matchingBracket = decode.findMatchingClosingBracket(input, 43);
-    expect(matchingBracket).toBe(45);
-
-    matchingBracket = decode.findMatchingClosingBracket(input, 8);
-    expect(matchingBracket).toBe(48);
+    expect(decode.findMatchingClosingBracket(input, 14)).toBe(33);
+    expect(decode.findMatchingClosingBracket(input, 43)).toBe(45);
+    expect(decode.findMatchingClosingBracket(input, 8)).toBe(48);
 });
 
-test('decode peterish string to "Hello World!"', () => {
+test('decode boopa-dee-bappa-dee string to "Hello World!"', () => {
     const input = 'pe pe pe pe pe pe pe pe boopadee boopa pe pe pe pe boopadee boopa pe pe boopa pe pe pe boopa pe pe pe boopa pe bappa bappa bappa bappa dee bappadee boopa pe boopa pe boopa dee boopa boopa pe boopadee bappa bappadee bappa dee bappadee boopa boopa boo boopa dee dee dee boo pe pe pe pe pe pe pe boo boo pe pe pe boo boopa boopa boo bappa dee boo bappa boo pe pe pe boo dee dee dee dee dee dee boo dee dee dee dee dee dee dee dee boo boopa boopa pe boo boopa pe pe boo';
-    const output = decode.decode(input);
-    expect(output).toBe('Hello World!\n');
+    expect(decode.decode(input)).resolves.toBe('Hello World!\n');
 });
 
-test('decode peterish string which uses cell values below zero to "Hello World!"', () => {
+test('decode boopa-dee-bappa-dee string which uses cell values below zero to "Hello World!"', () => {
     const input = 'boopa pe pe pe pe pe pe pe pe boopadee dee bappa pe pe pe pe pe pe pe pe pe boopa bappadee bappa boo boopa boopa pe boopa dee boopadee pe bappadee pe pe boopa pe pe boopa pe pe pe boopadee boopa boopadee dee boopa pe pe pe bappa bappa pe pe pe boopa bappadee bappa bappa bappadee boopa dee dee dee dee dee boo boopa dee boopa pe pe pe boo boo pe pe pe boo boopa dee boo bappa bappa pe boopadee boopa boopadee pe boopa pe bappadee boopa boopa bappadee bappa dee dee dee dee dee dee dee dee dee dee dee dee dee dee boo boopa boopa boo pe pe pe boo dee dee dee dee dee dee boo dee dee dee dee dee dee dee dee boo boopa pe boo boopa pe boo';
-    const output = decode.decode(input);
-    expect(output).toBe('Hello World!\n');
+    expect(decode.decode(input)).resolves.toBe('Hello World!\n');
+});
+
+test('boopadee string gets rejected because of unbalanced brackets', () => {
+    var input = "";
+
+    input = 'boopa boopadee boopadee boopadeepe pe pe pe pe pe pe pe boopadee dee bappa pe pe pe pe pe pe pe pe pe boopa boopadee bappadee bappa boo boopa boopa pe boopa dee boopadee pe bappadee pe pe boopa pe pe boopa pe pe pe boopadee boopa boopadee dee boopa pe pe pe bappa bappa pe pe pe boopa bappadee bappa bappa bappadee boopa dee dee dee dee dee boo boopa dee boopa pe pe pe boo boo pe pe pe boo boopa dee boo bappa bappa pe boopadee boopa boopadee pe boopa pe bappadee boopa boopa bappadee bappa dee dee dee dee dee dee dee dee dee dee dee dee dee dee boo boopa boopa boo pe pe pe boo dee dee dee dee dee dee boo dee dee dee dee dee dee dee dee boo boopa pe boo boopa pe boo';
+    expect(() => { decode.validate(input); }).toThrowError(/unbalanced/);
+
+    input = 'pe pe pe pe pe pe pe pe boopadee boopa pe pe pe pe boopadee bappadee boopa pe pe boopa pe pe pe boopa pe pe pe boopa pe bappa bappa bappa bappa dee bappadee boopa pe boopa pe boopa dee boopa boopa pe boopadee bappa bappadee bappa dee bappadee boopa boopa boo boopa dee dee dee boo pe pe pe pe pe pe pe boo boo pe pe pe boo boopa boopa boo bappa dee boo bappa boo pe pe pe boo dee dee dee dee dee dee boo dee dee dee dee dee dee dee dee boo boopa boopa pe boo boopa pe pe boo';
+    expect(() => { decode.validate(input); }).toThrowError(/unbalanced/);
 });
 
 /*
-test('decode another peterish string to "Hello World!"', () => {
+test('decode another boopa-dee-bappa-dee string to "Hello World!"', () => {
     const input = 'pe boopadee dee dee boopa dee boopadee boopa boopa pe boopa dee dee dee dee dee bappa bappa bappadee bappa dee dee bappa dee dee dee bappadee boopa dee boo boopa boopa boopa pe boo boopa boopa boo boo pe pe pe boopadee boo boopa bappadee bappa bappa bappa bappa boo pe pe pe boo dee dee dee dee dee dee boo bappa bappa dee boo boopa boopa boopa boopa pe boo';
-    const output = decode.decode(input);
-    expect(output).toBe('Hello World!\n');
+    expect(decode.decode(input)).resolves.toBe('Hello World!\n');
 });
 */
 
 /*
-test('decode peterish string which needs four cells to the left of the starting point to "Hello World!"', () => {
+test('decode boopa-dee-bappa-dee string which needs four cells to the left of the starting point to "Hello World!"', () => {
     const input = 'dee dee bappa dee bappa bappa pe boopadee pe boopadee bappa pe boopa dee dee dee boopa dee boopa dee boopa dee bappa bappa bappa bappadee boopa bappadee bappa bappa dee dee boo bappa pe pe pe pe pe pe boo bappa bappa dee boo boo bappa bappa boo bappa pe boo boopa boopa boo boopa boopa boo bappa bappa bappa boo pe pe pe boo boopa boopa boo boopa boopa dee boo bappa bappa bappa pe boo';
-    const output = decode.decode(input);
-    expect(output).toBe('Hello World!\n');
+    expect(decode.decode(input)).resolves.toBe('Hello World!\n');
 });
 */
